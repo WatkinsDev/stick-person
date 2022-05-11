bdc28885dff4930ca30f41fe7fdb31476c5993e7
Chris Watkins <chris.watkins@Statsbomb.com>
2022-05-11 15:44:17 +0100

    Working combination but needs transparency

--- cv/image_combine.py
+++ cv/image_combine.py
@@ -1,16 +1,20 @@
 import cv2
+import numpy as np
 
 ball_path = "./frames_output/img-0001_ball.png"
 stick_path = "./frames_output/img-0001_stick.png"
 
-img1 = cv2.imread(ball_path)
-img2 = cv2.imread(stick_path)
+img2 = cv2.imread(ball_path)
+img1 = cv2.imread(stick_path)
 
-one = img1.resize((220, 180))
-two = img2.resize((220, 180))
+dim = (img1.shape[1], img1.shape[0])
+resized = cv2.resize(img2, dim, interpolation=cv2.INTER_AREA)
 
-dst = cv2.addWeighted(one, 0.5, two, 0.7, 0)
 
-cv2.imshow("Blended Image", dst)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.imwrite("./frames_output/img-0001_resized.png", resized)
+
+# vis = np.concatenate((img1, resized), axis=1)
+# cv2.imwrite("./frames_output/img-0001_final.png", vis)
+
+added_image = cv2.addWeighted(img1, 0.5, resized, 0.5, 0)
+cv2.imwrite("./frames_output/img-0001_final.png", added_image)
