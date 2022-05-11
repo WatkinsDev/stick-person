4fef5723df4aaeef0205c71e663fe7f1c0ca2e67
Chris Watkins <chris.watkins@Statsbomb.com>
2022-05-11 16:29:54 +0100

    Combination working

--- cv/image_combine.py
+++ cv/image_combine.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-ball_path = "./frames_output/img-0001_ball.png"
+ball_path = "./frames_output/img-0001_ball_without_black.png"
 stick_path = "./frames_output/img-0001_stick.png"
 
 img2 = cv2.imread(ball_path)
@@ -10,11 +10,5 @@ img1 = cv2.imread(stick_path)
 dim = (img1.shape[1], img1.shape[0])
 resized = cv2.resize(img2, dim, interpolation=cv2.INTER_AREA)
 
-
-cv2.imwrite("./frames_output/img-0001_resized.png", resized)
-
-# vis = np.concatenate((img1, resized), axis=1)
-# cv2.imwrite("./frames_output/img-0001_final.png", vis)
-
-added_image = cv2.addWeighted(img1, 0.5, resized, 0.5, 0)
+added_image = cv2.addWeighted(img1, 1, resized, 1, 0)
 cv2.imwrite("./frames_output/img-0001_final.png", added_image)
